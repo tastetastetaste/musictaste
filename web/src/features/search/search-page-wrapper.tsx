e9b7f242465df6bdee1f924d2740b823afaf42d2
Yaser <76593995+900dluck@users.noreply.github.com>
2025-10-24 20:18:59 +0300

    impr(web/search): use debounced search value in search page

--- web/src/features/search/search-page-wrapper.tsx
+++ web/src/features/search/search-page-wrapper.tsx
@@ -1,7 +1,8 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Outlet, useSearchParams } from 'react-router-dom';
 import AppPageWrapper from '../../layout/app-page-wrapper';
 import { Navigation } from '../../components/nav';
+import { useDebounce } from '../../hooks/useDebounce';
 import { SearchInput } from './search-input';
 
 export type SearchPageOutletContext = {
@@ -11,38 +12,61 @@ export type SearchPageOutletContext = {
 const SearchPageWrapper: React.FC = () => {
   const [params, setParams] = useSearchParams();
 
-  const [q, setQ] = useState(params.get('q'));
+  const [q, setQ] = useState(params.get('q') || '');
+  const debouncedValue = useDebounce(q, 300);
+
+  useEffect(() => {
+    if (debouncedValue) {
+      setParams({ q: debouncedValue });
+    } else {
+      setParams({});
+    }
+  }, [debouncedValue, setParams]);
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setQ(e.target.value);
+  };
 
   return (
     <AppPageWrapper title="Search">
       <SearchInput
         placeholder="Search..."
         value={q}
-        onChange={(e) => setQ(e.target.value)}
+        onChange={handleInputChange}
       />
       <Navigation
         links={[
-          { label: 'All', to: `/search${q ? '?q=' + q : ''}` },
+          {
+            label: 'All',
+            to: `/search${debouncedValue ? '?q=' + debouncedValue : ''}`,
+          },
           {
             label: 'Release',
-            to: `/search/release${q ? '?q=' + q : ''}`,
+            to: `/search/release${debouncedValue ? '?q=' + debouncedValue : ''}`,
           },
           {
             label: 'Artist',
-            to: `/search/artist${q ? '?q=' + q : ''}`,
+            to: `/search/artist${debouncedValue ? '?q=' + debouncedValue : ''}`,
           },
           {
             label: 'Label',
-            to: `/search/label${q ? '?q=' + q : ''}`,
+            to: `/search/label${debouncedValue ? '?q=' + debouncedValue : ''}`,
           },
           {
             label: 'Genre',
-            to: `/search/genre${q ? '?q=' + q : ''}`,
+            to: `/search/genre${debouncedValue ? '?q=' + debouncedValue : ''}`,
           },
-          { label: 'User', to: `/search/user${q ? '?q=' + q : ''}` },
+          {
+            label: 'User',
+            to: `/search/user${debouncedValue ? '?q=' + debouncedValue : ''}`,
+          },
         ]}
       />
-      {q ? <Outlet context={{ q }} /> : <div></div>}
+      {debouncedValue ? (
+        <Outlet context={{ q: debouncedValue }} />
+      ) : (
+        <div></div>
+      )}
     </AppPageWrapper>
   );
 };
