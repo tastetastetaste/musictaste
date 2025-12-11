4bb7d1220d960bcdc8a8cd21fbebeff10d69787a
Yaser <76593995+900dluck@users.noreply.github.com>
2025-12-11 09:53:51 +0300

    chore(releases): make full date end of period for partial dates

--- api/src/common/normalizeDate.ts
+++ api/src/common/normalizeDate.ts
@@ -1,17 +1,16 @@
 import dayjs from 'dayjs';
 import { DatePrecision } from 'shared';
-
 export function normalizeDate(input: string) {
   if (/^\d{4}$/.test(input)) {
     return {
       date: dayjs(input).format('YYYY'),
-      fullDate: dayjs(input).format('YYYY-MM-DD'),
+      fullDate: dayjs(input).endOf('year').format('YYYY-MM-DD'),
       precision: DatePrecision.YEAR,
     };
   } else if (/^\d{4}-\d{2}$/.test(input)) {
     return {
       date: dayjs(input).format('YYYY-MM'),
-      fullDate: dayjs(input).format('YYYY-MM-DD'),
+      fullDate: dayjs(input).endOf('month').format('YYYY-MM-DD'),
       precision: DatePrecision.MONTH,
     };
   } else {
