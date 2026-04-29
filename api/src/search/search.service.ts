c65ea49456e90f110c145e948930a6b1152d9cd7
owen hanson <ambrynhanson@gmail.com>
2026-04-28 20:46:00 -0400

    normalizes text but buggy

--- api/src/search/search.service.ts
+++ api/src/search/search.service.ts
@@ -28,7 +28,15 @@ export class SearchService {
     page,
     pageSize,
   }: SearchDto): Promise<ISearchResponse> {
-    const q = query.trim();
+    const normalizeQuery = (q: string) => {
+      q = q.toLowerCase();
+      q = q.replace(/[^\w\s]/g, '');
+      q = q.replace(/\s+/g, ' ');
+      q = q.trim();
+      return q;
+    };
+
+    const q = normalizeQuery(query);
     const curPageSize = pageSize || 10;
     const take = (page || 1) * curPageSize;
     const skip = ((page || 1) - 1) * curPageSize;
